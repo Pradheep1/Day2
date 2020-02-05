ec5ab6de0592a8d94c7a279ef045ef07a76e1f80
Pradheep1 <pradheepre@outlook.com>
2020-02-05 10:27:15 +0530

    Added

--- src/org/college/College.java
+++ src/org/college/College.java
@@ -1,14 +1,9 @@
 package org.college;
 
-import java.util.Scanner;
 
 public class College {
 public static void main(String[] args) {
-	@SuppressWarnings("resource")
-	Scanner S = new Scanner(System.in);
-	System.out.println("empId");
-	String S1 = S.nextLine();
-	System.out.println(S1);
+System.out.println("Hi");	
 }
 	
 	
