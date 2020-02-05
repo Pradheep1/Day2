ff8207974bc93e41a74a823dfff2cce8033be0f2
Pradheep1 <pradheepre@outlook.com>
2020-02-05 10:13:14 +0530

    Success

---
+++ src/org/college/College.java
@@ -0,0 +1,15 @@
+package org.college;
+
+import java.util.Scanner;
+
+public class College {
+public static void main(String[] args) {
+	@SuppressWarnings("resource")
+	Scanner S = new Scanner(System.in);
+	System.out.println("empId");
+	String S1 = S.nextLine();
+	System.out.println(S1);
+}
+	
+	
+}
